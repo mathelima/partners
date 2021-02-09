188ec17841c6ed56dc2602a33dffde36c33111fd
Matheus <math.lima.andrade@gmail.com>
2021-02-09 11:34:52 -0300

    changed type hint

--- src/dao/base_dao.py
+++ src/dao/base_dao.py
@@ -6,7 +6,7 @@ class BaseDao:
     def __init__(self, type_model) -> None:
         self.__type_model = type_model
 
-    def save(self, model: BaseModel) -> None:
+    def save(self, model: BaseModel) -> BaseModel:
         with Session() as session:
             session.add(model)
             session.commit()
